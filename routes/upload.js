5f89b9abeaa29341730855d85bd5ff73f327e0e1
Deagnon <d.money.deagnon@gmail.com>
2026-05-18 14:00:26 +0100

    z

--- routes/upload.js
+++ routes/upload.js
@@ -5,8 +5,9 @@ const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 
 // Cloudinary config
+// Support two env var naming conventions: CLOUDINARY_NAME or CLOUDINARY_CLOUD_NAME
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME || process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
