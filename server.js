fff81043a5921b359033005434ce8b4b0e2e63b6
Deagnon <d.money.deagnon@gmail.com>
2026-05-24 16:32:15 +0100

    z

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/messages', require('./routes/messages'));
-app.use('/api/groups', require('./routes/groups'));
+
 app.use('/api/upload', require('./routes/upload'));
 
 
